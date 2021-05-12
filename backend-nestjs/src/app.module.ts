653356ed3dc976bac6dd5c4bb35502a9d19b7621
Manassarn Manoonchai <manassarn@gmail.com>
2021-05-12 13:16:47 +0700

    :sparkles: (backend): Add config module with dotenv-like file pattern

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const nodeEnv = process.env.NODE_ENV || 'development';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${nodeEnv}.local`, `.env.${nodeEnv}`, '.env'],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
